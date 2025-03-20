606ca8924dacd6e8bb3d7bcb452dfb3dbfd8f7a9
victoriaviv <dufatanyevictoria2@gmail.com>
2025-03-20 10:14:08 +0200

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,40 @@
-import React from "react";
-import { Outlet,Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import { useState } from "react";
-import "../styles/navbar.css"
-
+import "../styles/navbar.css";
 import Login from "../components/Login.jsx";
-function Navbar(){
+import { FaBars, FaTimes } from "react-icons/fa";
 
-  const [modal,setModal] = useState(false);
+function Navbar() {
+  const [modal, setModal] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-const changeModal = () => {
-  setModal(!modal)
-}
+  const changeModal = () => {
+    setModal(!modal);
+  };
 
-    return(
-        <div >
-         {modal && <Login changeModal = {changeModal}/>}
+  return (
+    <div>
+      {modal && <Login changeModal={changeModal} />}
       <div className="navbar">
-      <div className="logo"><img src={logo} alt="logo"/></div>
-      <ul>
-     
-       <li><Link to="/">Home</Link></li>
-       <li><Link to="/Pages">Pages</Link></li>
-       <li><Link to="/Service">Service</Link></li>
-       <li><Link to="/Blog">Blog</Link></li>
-       <li><Link to="/Contact">Contact</Link></li>
-       {/* <FaUserCircle className="user" /> */}
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
        
-       </ul>
-       <button className="login"onClick={changeModal}>login</button>
-      
-       </div>
-     </div>
-      
-    )
+        <ul className={menuOpen ? "nav-links open" : "nav-links"}>
+          <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+          <li><Link to="/Pages" onClick={() => setMenuOpen(false)}>Pages</Link></li>
+          <li><Link to="/Service" onClick={() => setMenuOpen(false)}>Service</Link></li>
+          <li><Link to="/Blog" onClick={() => setMenuOpen(false)}>Blog</Link></li>
+          <li><Link to="/Contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+        </ul>
+        <button className="login" onClick={changeModal}>Login</button>
+        <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Navbar;
